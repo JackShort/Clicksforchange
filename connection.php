b09eacd52974b3251a21debf3f89e0fbe4268b09
John Short <john.short.tj@gmail.com>
2015-09-26 16:32:28 -0400

    Fixed connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,23 @@
-<?php 
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+<?php
+	// // $servername = "localhost";
+	// // $username = "root";
+	// // $password = "";
+	// $db = "clickforchange_db";
 
-	$server = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
-	$db = substr($url["path"], 1);
+	// //create connection
+	// $conn = mysql_connect($servername, $username, $password)
+	// 	or die("Could not connect");
+	// $selected = mysql_select_db($db);
 
-	$conn = new mysql($server, $username, $password, $db);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = mysql_connect($server, $username, $password)
+	or die("Could not connect");
+$selected = mysql_select_db($db);
+// $conn = new mysqli($server, $username, $password, $db);
 ?>
