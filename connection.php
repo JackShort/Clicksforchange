28cc84ff1f0434d9e3a1c24af50ba34d23c86f88
John Short <john.short.tj@gmail.com>
2015-09-26 17:29:07 -0400

    Fixed connection.php

--- connection.php
+++ connection.php
@@ -1,23 +1,25 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db = "clickforchange_db";
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+	// $db = "clickforchange_db";
 
-	//create connection
-	$conn = mysql_connect($servername, $username, $password)
-		or die("Could not connect");
-	$selected = mysql_select_db($db);
+	// //create connection
+	// $conn = mysql_connect($servername, $username, $password)
+	// 	or die("Could not connect");
+	// $selected = mysql_select_db($db);
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-// $conn = mysql_connect($server, $username, $password)
-// 	or die("Could not connect");
-// $selected = mysql_select_db($db);
+$conn = mysql_connect($server, $username, $password)
+	or die("Could not connect");
+$selected = mysql_select_db($db);
+
+//don't touch this one
 // $conn = new mysqli($server, $username, $password, $db);
 ?>
