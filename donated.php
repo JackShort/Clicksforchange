5802f6243ae03eee5d7cee15ebe72862d0edfb42
John Short <john.short.tj@gmail.com>
2015-09-27 00:55:34 -0400

    Added favicon

--- donated.php
+++ donated.php
@@ -81,6 +81,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Clicks For Change</title>
 
+		<link rel="shortcut icon" href="../images/favicon.ico">
+		<link rel="apple-touch-icon" href="../images/favicon.ico">
+
 		<!-- latest compiled and minified css -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
