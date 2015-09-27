8d8571262259cb99f86628aa88d5f793cea6a1ec
John Short <john.short.tj@gmail.com>
2015-09-27 01:10:05 -0400

    Favicon fix

--- about.php
+++ about.php
@@ -4,6 +4,9 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Clicks 4 Change</title>
+		
+		<link rel="shortcut icon" href="../images/favicon.ico">
+		<link rel="apple-touch-icon" href="../images/favicon.ico">
 
 		<!-- latest compiled and minified css -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
