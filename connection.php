8b2583da01a8c04f684ea1a54eee2fb30f253f38
John Short <john.short.tj@gmail.com>
2015-09-28 09:17:32 -0400

    Just a test

--- connection.php
+++ connection.php
@@ -1,24 +1,24 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db = "clickforchange_db";
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+	// $db = "clickforchange_db";
 
-	//create connection
-	$conn = mysql_connect($servername, $username, $password)
-		or die("Could not connect");
-	$selected = mysql_select_db($db);
-
-	// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-	// $server = $url["host"];
-	// $username = $url["user"];
-	// $password = $url["pass"];
-	// $db = substr($url["path"], 1);
-
-	// $conn = mysql_connect($server, $username, $password)
+	// //create connection
+	// $conn = mysql_connect($servername, $username, $password)
 	// 	or die("Could not connect");
 	// $selected = mysql_select_db($db);
+
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$conn = mysql_connect($server, $username, $password)
+		or die("Could not connect");
+	$selected = mysql_select_db($db);
 
 //don't touch this one
 // $conn = new mysqli($server, $username, $password, $db);
