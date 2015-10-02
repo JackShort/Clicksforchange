85bea0015eb27bcb8abc7b14473cab7d6a69a652
John Short <john.short.tj@gmail.com>
2015-10-02 13:56:00 -0400

    This should fix it

--- index.php
+++ index.php
@@ -102,12 +102,10 @@
 		  ga('create', 'UA-68266810-1', 'auto');
 		  ga('send', 'pageview');
 		</script>
-		
-		<script type="text/javascript" src="//go.oclasrv.com/apu.php?zoneid=409031"></script>
 	</head>
 
-	<body style="">
-
+	<body>
+	<script type='text/javascript' src='//go.oclasrv.com/apu.php?zoneid=409031'></script>
 
 		<div class="container-fluid bottom-buffer full-screen">
 
