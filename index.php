7c99f219802415620b91ee8364010328f615a95a
John Short <john.short.tj@gmail.com>
2015-10-07 09:29:45 -0400

    Propeller ads added

--- index.php
+++ index.php
@@ -107,6 +107,30 @@
 		  ga('create', 'UA-68266810-1', 'auto');
 		  ga('send', 'pageview');
 		</script>
+
+		<!-- PopAds.net Popunder Code for www.clicks4change.com -->
+		<script type="text/javascript">
+		  var _pop = _pop || [];
+		  _pop.push(['siteId', 887947]);
+		  _pop.push(['minBid', 0.000000]);
+		  _pop.push(['popundersPerIP', 0]);
+		  _pop.push(['delayBetween', 0]);
+		  _pop.push(['default', false]);
+		  _pop.push(['defaultPerDay', 0]);
+		  _pop.push(['topmostLayer', false]);
+		  (function() {
+		    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+		    var s = document.getElementsByTagName('script')[0]; 
+		    pa.src = '//c1.popads.net/pop.js';
+		    pa.onerror = function() {
+		      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+		      sa.src = '//c2.popads.net/pop.js';
+		      s.parentNode.insertBefore(sa, s);
+		    };
+		    s.parentNode.insertBefore(pa, s);
+		  })();
+		</script>
+		<!-- PopAds.net Popunder Code End -->
 	</head>
 
 	<body>
